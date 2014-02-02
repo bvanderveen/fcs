3360ca648c0aecb28a775cc259005aa7462eddbf
Benjamin van der Veen <b@bvanderveen.com>
2014-02-02 11:37:48 -0800

    bounded roll angle

--- sim/src/sim/course.clj
+++ sim/src/sim/course.clj
@@ -8,13 +8,20 @@
    :longitude 0
    :velocity 0.0003
    :heading 0
-   :heading-rate 0 })
+   :roll 0
+   })
 
-(defn course-fn [state turn dt]
+(defn bound
+  ([n low high] (min (max n low) high))
+  ([n absolute] (bound n (* -1 absolute) absolute)))
+
+(defn course-fn [state aileron elevator dt]
   {
    :latitude (+ (:latitude state) (* dt (math/cos (:heading state)) (:velocity state)))
    :longitude (+ (:longitude state) (* dt (math/sin (:heading state)) (:velocity state)))
    :velocity (:velocity state)
-   :heading (+ (:heading state) (* dt (:heading-rate state)))
-   :heading-rate (+ 0.000003 (if (nil? turn) 0 turn) (:heading-rate state))
+   :heading (+ (:heading state) (* dt (:roll state)))
+   :roll (bound
+          (+ (:roll state) (* dt aileron))
+          (/ Math/PI 6)) ; don't bank past 30 degrees
    })
